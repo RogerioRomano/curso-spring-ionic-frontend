03e5839bb41adaccc8de7e224268d1c2f58944a8
RogerioRomano <rogerio_romano@hotmail.com>
2020-01-24 19:49:09 -0300

    Mostrando dados e imagem do cliente na pagina de profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CategoriaService } from '../services/domain/categoria.service';
 import { ErrorInterceptorProvider } from '../intecerptors/error-interceptor';
 import { AuthService } from '../services/auth.service';
 import { StorageService } from '../services/storage.service';
+import { ClienteService } from '../services/domain/cliente.service';
 
 /**
  * import { HomePage } from '../pages/home/home';
@@ -72,7 +73,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     CategoriaService,
     ErrorInterceptorProvider,
     AuthService,
-    StorageService
+    StorageService, 
+    ClienteService
   ]
 })
 export class AppModule {}
