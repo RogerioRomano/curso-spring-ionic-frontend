cfe293bf56c9fd0a921c08c7e3db32f101330f2a
RogerioRomano <rogerio_romano@hotmail.com>
2020-01-26 15:41:40 -0300

    Povoando dinamicamente estado e cidade

--- src/services/domain/estado.service.ts
+++ src/services/domain/estado.service.ts
@@ -1,19 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { API_CONFIG } from "../../app/config/api.config";
-import { CategoriaDTO } from "../../models/categoria.dto";
+import { EstadoDTO } from "../../models/estado.dto";
 import { Observable } from "rxjs/Rx";
 
 /**
  * O decorator @Injectable() habilita a capacidade de injeção da classe em outras classes.
  */
 @Injectable()
-export class CategoriaService {
+export class EstadoService {
 
     constructor(public http: HttpClient) {
     }
 
-    findAll() : Observable<CategoriaDTO[]> {
-        return this.http.get<CategoriaDTO[]>(`${API_CONFIG.baseUrl}/categorias`);
+    findAll() : Observable<EstadoDTO[]> {
+        return this.http.get<EstadoDTO[]>(`${API_CONFIG.baseUrl}/estados`);
     }
 }
